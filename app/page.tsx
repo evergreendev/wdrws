98d284d6dc64e56ba66c359be936f8c66f72e677
Joe simpson <joe@egmrc.com>
2024-03-14 10:00:13 -0600

    Size down home page

--- app/page.tsx
+++ app/page.tsx
@@ -27,34 +27,34 @@ export default async function Home() {
     return (
         <main className="font-pt_sans flex min-h-screen flex-col justify-between bg-white">
             <div className="bg-secondary-100 pb-28 ml-24 flex justify-start font-newsreader">
-                <ImageCard wide linkStyles="w-6/12 -translate-x-24" colorScheme="yellow" src={reportImg}
+                <ImageCard wide linkStyles="w-6/12 max-w-screen-lg -translate-x-24" colorScheme="yellow" src={reportImg}
                            link="/annual-report" text="Read the WDRWS Annual Report"/>
-                <div className="z-20 -ml-14">
-                    <h2 className="mt-36 text-9xl mb-8 -ml-40">purpose</h2>
-                    <p className="text-6xl font-light max-w-screen-md">
-                        Ensuring quality, abundant water to all corners of<br/> western South Dakota
+                <div className="z-20 -ml-36">
+                    <h2 className="mt-56 text-7xl mb-4">purpose</h2>
+                    <p className="text-4xl ml-14 font-light max-w-screen-sm">
+                        Ensuring quality, abundant water to all corners of western South Dakota
                     </p>
                 </div>
             </div>
-            <div className="-mt-16 ml-24 w-11/12 flex font-newsreader">
-                <ImageCard colorScheme="blue" src={opportunityImg} linkStyles="w-5/12 mb-24"
+            <div className="-mt-16 ml-24 w-11/12 max-w-screen-2xl self-center flex font-newsreader">
+                <ImageCard colorScheme="blue" src={opportunityImg} linkStyles="w-5/12 max-w-lg mb-24"
                            text="Learn more about the water needs of western South Dakota" link="/about"/>
                 <div className="bg-black bg-opacity-20 border-t-[64px] border-b-[64px] w-0.5 mx-7 z-20"/>
                 <div>
-                    <h2 className="mt-36 text-9xl mb-12">opportunity</h2>
-                    <p className="text-6xl font-light max-w-screen-lg ml-7">
-                        We have the opportunity to <br/>prevent water access challenges <br/>for future generations.
+                    <h2 className="mt-36 text-7xl mb-4">opportunity</h2>
+                    <p className="text-4xl font-light max-w-screen-md ml-7">
+                        We have the opportunity to prevent water access challenges for future generations.
                     </p>
                 </div>
             </div>
-            <div className="bg-secondary-100 justify-end -mt-16 w-10/12 flex font-newsreader pb-28">
-                <div className="text-right mr-12">
-                    <h2 className="mt-14 text-9xl mb-12">progress</h2>
-                    <p className="text-6xl font-light max-w-screen-lg ml-7">
+            <div className="bg-secondary-100 justify-end -mt-16 w-9/12 flex font-newsreader pb-28">
+                <div className="text-right mr-32 mt-7">
+                    <h2 className="mt-14 text-7xl mb-4">progress</h2>
+                    <p className="text-4xl font-light max-w-screen-lg ml-7">
                         WDRWS was formed in 2021 to <br/>address the growing water needs <br/>of western South Dakota.
                     </p>
                 </div>
-                <ImageCard linkStyles="w-4/12 -mt-24" colorScheme="yellow" src={discoverImg}
+                <ImageCard linkStyles="w-4/12 max-w-lg max-w-screen-sm -mt-24" colorScheme="yellow" src={discoverImg}
                            text="Discover what the WDRWS has accomplished so far"
                            link="/discover"/>{/*todo all links on homepage*/}
             </div>
