48aaa77d9b5b27831a0eb6c21677e2bb3c1c82e5
Joe simpson <joe@egmrc.com>
2024-04-05 15:29:08 -0600

    Fix mobile styling for footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,31 +4,81 @@ import Link from "next/link";
 import evergreenLogo from "@/public/evergreen.png";
 import facebookLogo from "@/public/facebook-icon.png";
 import linkedInLogo from "@/public/linkedin.png";
+import ColCard from "@/app/components/ColCard";
+import materialsImg from "@/public/technical-sessions.png";
+import discoverImg from "@/public/discover.jpg";
+import newsImg from "@/public/gary-drewes.jpg";
+import aboutImg from "@/public/katie-leclair.jpg";
 
 const Footer = () => {
     const currentYear = new Date().getFullYear();
-    return <div className="bg-secondary-500 pb-7 text-secondary-100 uppercase">
-        <div className="w-full max-w-screen-xl m-auto flex justify-between items-center">
-            <div className="flex items-center">
-                <div>
-                    <Link href="/">
-                        <Image className="w-80 p-4" src={mainLogo} alt="Western Dakota Regional Water System"/>
+    return <>
+        <div className="w-full max-w-screen-xl m-auto -translate-y-20 flex flex-wrap justify-around">
+            <ColCard styles="lg:w-[31%] max-w-96 w-full mb-7" textClass="text-primary-500" borderClass="border-primary-500"
+                     src={materialsImg} header="Materials" subHeader="Technical Sessions"
+                     text="The anatomy of regional water projects" items={[
+                {
+                    title: "Annual Report",
+                    url: "/annual-report",
+                    src: discoverImg
+                },
+                {
+                    title: "Meeting Agenda",
+                    url: "/meeting-agenda",
+                    src: discoverImg
+                }
+            ]}/>
+            <ColCard styles="lg:w-[31%] max-w-96 w-full mb-7" textClass="text-secondary-500" borderClass="border-t-secondary-500"
+                     src={newsImg} header="News" subHeader="In the Media" text="Congratulations to @Gary Drewes for being appointed to the SD State Water Management Board
+" items={[
+                {
+                    title: "Events",
+                    url: "/events",
+                    src: discoverImg
+                }
+            ]}/>
+            <ColCard styles="lg:w-[31%] max-w-96 w-full mb-7" textClass="text-green-500" borderClass="border-green-500" src={aboutImg}
+                     header="About Us" subHeader="Team Profile"
+                     text="Introducing Katie LeClair, the new Operations Manager of WDRWS" items={[
+                {
+                    title: "The Board",
+                    url: "/the-board",
+                    src: discoverImg
+                },
+                {
+                    title: "Our History",
+                    url: "/our-history",
+                    src: discoverImg
+                }
+            ]}/>
+        </div>
+        <div className="bg-secondary-500 pb-7 text-secondary-100 uppercase">
+            <div className="w-full max-w-screen-xl m-auto flex flex-wrap justify-around lg:justify-between items-center px-2">
+                <div className="flex flex-wrap items-center">
+                    <div>
+                        <Link href="/">
+                            <Image className="w-80 p-4" src={mainLogo} alt="Western Dakota Regional Water System"/>
+                        </Link>
+                        <p className="pl-4 opacity-60">Copyright WDRWS {currentYear}</p>
+                    </div>
+                    <Link className="mx-2" href="https://www.facebook.com/westsdwater">
+                        <Image className="w-11" src={facebookLogo}
+                               alt="Follow Western Dakota Regional Water System on Facebook"/>
                     </Link>
-                    <p className="pl-4 opacity-60">Copyright WDRWS {currentYear}</p>
+                    <Link className="mx-2" href="/">
+                        <Image className="w-11" src={linkedInLogo}
+                               alt="Follow Western Dakota Regional Water System on LinkedIn"/>
+                    </Link>
                 </div>
-                <Link className="mx-2" href="https://www.facebook.com/westsdwater">
-                    <Image className="w-11" src={facebookLogo} alt="Follow Western Dakota Regional Water System on Facebook"/>
-                </Link>
-                <Link className="mx-2" href="/">
-                    <Image className="w-11" src={linkedInLogo} alt="Follow Western Dakota Regional Water System on LinkedIn"/>
+                <Link className="w-80 block mt-8 max-w-[50%] opacity-60 hover:opacity-85 transition-opacity"
+                      href="https://evergreenmediarc.com/">
+                    <p className="text-right">Design</p>
+                    <Image className="w-full" src={evergreenLogo} alt="Evergreen Media"/>
                 </Link>
             </div>
-            <Link className="w-80 block opacity-60 hover:opacity-85 transition-opacity" href="https://evergreenmediarc.com/">
-                <p className="text-right">Design</p>
-                <Image className="w-full" src={evergreenLogo} alt="Evergreen Media"/>
-            </Link>
         </div>
-    </div>
+    </>
+
 }
 
 export default Footer;
--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,6 @@ import ImageCard from "@/app/components/ImageCard";
 import reportImg from "@/public/annual-report.jpg";
 import opportunityImg from "@/public/opportunity.jpg";
 import discoverImg from "@/public/discover.jpg";
-import aboutImg from "@/public/katie-leclair.jpg";
-import newsImg from "@/public/gary-drewes.jpg";
-import materialsImg from "@/public/technical-sessions.png";
-import ColCard from "@/app/components/ColCard";
 import Footer from "@/app/components/Footer";
 import {CMS_URL} from "@/constants";
 
@@ -48,54 +44,16 @@ export default async function Home() {
                 </div>
             </div>
             <div className="bg-green-200 justify-end md:-mt-16 md:w-9/12 flex flex-wrap lg:flex-nowrap items-start font-newsreader pb-28">
-                <div className="md:text-right w-full md:mr-32 mt-7 bg-green-400 border-l-green-500 border-l-8 p-4 pr-14 bg-opacity-50">
+                <div className="md:text-right w-full md:mr-32 mt-7 bg-green-400 border-l-green-500 border-l-8 p-4 sm:pr-14 bg-opacity-50">
                     <h2 className="mt-7 sm:text-7xl text-4xl mb-4">progress</h2>
                     <p className="sm:text-4xl text-lg font-light max-w-screen-lg ml-auto">
-                        WDRWS was formed in 2021 to <br/>address the growing water needs <br/>of western South Dakota.
+                        WDRWS was formed in 2021 to <br className="hidden sm:block"/>address the growing water needs <br className="hidden sm:block"/>of
+                        western South Dakota.
                     </p>
                 </div>
                 <ImageCard linkStyles="w-full lg:w-4/12 max-w-lg max-w-lg lg:-mt-24" colorScheme="yellow" src={discoverImg}
                            text="Discover what the WDRWS has accomplished so far"
                            link="/discover"/>{/*todo all links on homepage*/}
-            </div>
-            <div className="w-full max-w-screen-xl m-auto -translate-y-20 flex justify-between">
-                <ColCard styles="w-[31%]" textClass="text-primary-500" borderClass="border-primary-500"
-                         src={materialsImg} header="Materials" subHeader="Technical Sessions"
-                         text="The anatomy of regional water projects" items={[
-                    {
-                        title: "Annual Report",
-                        url: "/annual-report",
-                        src: discoverImg
-                    },
-                    {
-                        title: "Meeting Agenda",
-                        url: "/meeting-agenda",
-                        src: discoverImg
-                    }
-                ]}/>
-                <ColCard styles="w-[31%]" textClass="text-secondary-500" borderClass="border-t-secondary-500"
-                         src={newsImg} header="News" subHeader="In the Media" text="Congratulations to @Gary Drewes for being appointed to the SD State Water Management Board
-" items={[
-                    {
-                        title: "Events",
-                        url: "/events",
-                        src: discoverImg
-                    }
-                ]}/>
-                <ColCard styles="w-[31%]" textClass="text-green-500" borderClass="border-green-500" src={aboutImg}
-                         header="About Us" subHeader="Team Profile"
-                         text="Introducing Katie LeClair, the new Operations Manager of WDRWS" items={[
-                    {
-                        title: "The Board",
-                        url: "/the-board",
-                        src: discoverImg
-                    },
-                    {
-                        title: "Our History",
-                        url: "/our-history",
-                        src: discoverImg
-                    }
-                ]}/>
             </div>
             <Footer/>
         </main>
