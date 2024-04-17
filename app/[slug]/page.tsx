33b37df569a4003a2ef342dd2c165cb80433c068
Joe simpson <joe@egmrc.com>
2024-04-17 15:51:11 -0600

    Improve content page

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,7 +1,7 @@
 import {CMS_URL} from "@/constants";
 import {notFound} from "next/navigation";
 
-async function getData(slug:string) {
+async function getData(slug: string) {
     const res = await fetch(`${CMS_URL}pages?slug=${slug}`)
 
     if (!res.ok) {
@@ -11,20 +11,21 @@ async function getData(slug:string) {
     return res.json();
 }
 
-export default async function Page({ params }: { params: { slug: string } }) {
+export default async function Page({params}: { params: { slug: string } }) {
     const data = await getData(params.slug);
 
-    if (!data[0]){
+    if (!data[0]) {
         notFound();
     }
 
     return (
-        <main className="font-pt_sans flex min-h-screen bg-white">
-            <div className="mt-24 bg-secondary-100 w-full max-w-screen-2xl">
-                <div
-                    className="content bg-white bg-opacity-60 w-full max-w-screen-lg ml-auto -mr-36 -mt-12 mb-24 shadow-lg flex flex-col p-6 pt-12 border-t-8 border-primary-500"
-                    dangerouslySetInnerHTML={{__html: data[0].content.rendered}}/>
+        <main className="font-pt_sans flex-col bg-white">
+            <div className="bg-secondary-500 flex">
+                <h1 className="pl-7 py-7 text-white text-8xl font-newsreader lg:ml-auto lg:w-9/12">{data[0].title.rendered}</h1>
             </div>
+            <div
+                className="content mx-auto bg-white bg-opacity-60 w-full max-w-screen-lg shadow-lg flex flex-col p-6 pt-12"
+                    dangerouslySetInnerHTML={{__html: data[0].content.rendered}}/>
         </main>
     );
 }
