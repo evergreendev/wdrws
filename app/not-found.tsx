2e2383bbf70ad7309311e55bace25885d28160a2
Joe simpson <joe@egmrc.com>
2024-04-17 14:48:22 -0600

    Update not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,9 +3,11 @@ import Search from "@/app/components/Search";
 
 export default function NotFound() {
     return (
-        <div className="bg-white mx-auto p-6 py-14 my-3 shadow-xl w-full max-w-screen-sm">
-            <h2 className="font-avenir font-bold text-4xl">Not Found</h2>
-            <p className="text-lg mt-2">Could not find the requested page. It may have moved, or been removed.</p>
+        <div className="bg-white mx-auto p-6 py-14 my-3 shadow-xl w-full max-w-screen-lg">
+            <h2 className="font-newsreader text-4xl">Not Found</h2>
+            <p className="text-lg mt-2">We&apos;re sorry, but the page you&apos;re looking for can&apos;t be found. It may have been removed, renamed, or is temporarily unavailable.
+
+                Please double-check the URL or go to our homepage.</p>
             <div className="mt-5 ml-2">
                 <p className="mb-2">Try a search</p>
                 <Search dark />
