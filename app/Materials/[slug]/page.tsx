f5c7208dadb606fc50209716a917a00b7da9fa5b
Joe simpson <joe@egmrc.com>
2024-04-25 11:46:40 -0600

    Change cache

--- app/Materials/[slug]/page.tsx
+++ app/Materials/[slug]/page.tsx
@@ -13,21 +13,14 @@ async function getData(slug: string) {
 }
 
 export default async function Page({params}: { params: { slug: string } }) {
-    const data = await getData(params.slug);
 
-    if (!data[0]) {
-        console.log("cat slug empty")
-        notFound();
-    }
 
     return (
         <main className="font-pt_sans flex-col bg-white">
             <div className="flex">
-                <h1 className="bg-secondary-500 pl-7 py-7 text-white text-7xl font-newsreader lg:ml-auto lg:w-10/12">{data[0].title.rendered}</h1>
+                <h1 className="bg-secondary-500 pl-7 py-7 text-white text-7xl font-newsreader lg:ml-auto lg:w-10/12">t</h1>
             </div>
-            <div
-                className="content mx-auto bg-white bg-opacity-60 w-full max-w-screen-xl shadow-lg flex flex-col p-6 pt-12"
-                dangerouslySetInnerHTML={{__html: data[0].content.rendered}}/>
+            <div>This is it</div>
         </main>
     );
 }
