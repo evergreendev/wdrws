24642c73dc8c2041ba922dda6e1b8fdb4fbebeff
Joe simpson <joe@egmrc.com>
2024-08-29 15:26:31 -0600

    Add redirect

---
+++ app/annualmeeting/route.ts
@@ -0,0 +1,6 @@
+import {redirect} from "next/navigation";
+
+export async function GET() {
+    redirect(`https://www.eventbrite.com/e/western-dakota-regional-water-system-annual-meeting-tickets-912759286597?aff=oddtdtcreator`);
+}
+
