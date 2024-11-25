055550cad1d076d027414d7d173b953e1863dca5
Joe simpson <joe@egmrc.com>
2024-11-25 14:47:26 -0700

    Add ability to edit contact from CMS

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,9 +1,30 @@
 import InnerPageContent from "@/app/components/InnerPageContent";
 import ContactForm from "@/app/components/ContactForm";
+import {notFound} from "next/navigation";
+import {CMS_URL} from "@/constants";
+
+async function getData(slug: string) {
+    const res = await fetch(`${CMS_URL}pages?slug=${slug}`, { cache: 'no-store' })
+
+    if (!res.ok) {
+        notFound();
+    }
+
+    return res.json();
+}
 
 export default async function Page() {
+    const data = await getData("contact");
+
+    console.log(data[0])
+
+    if (!data[0]) {
+        notFound();
+    }
 
     return (
-        <InnerPageContent width={"MD"} content={<div className="py-10"><ContactForm/></div>} title="Contact"/>
+        <InnerPageContent width={"MD"} content={<div className="py-10"><ContactForm/>
+            <div className="mt-6" dangerouslySetInnerHTML={{__html: data[0].content.rendered}}/>
+        </div>} title="Contact"/>
     );
 }
