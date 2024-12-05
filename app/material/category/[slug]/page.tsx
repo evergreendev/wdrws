a7c6fb4436a6654c6d8d75cd74218876e1050b9c
Joe simpson <joe@egmrc.com>
2024-12-05 13:26:32 -0700

    Add page data fetching and refactor CSS styles
    Implemented a new function to fetch and display page-specific data dynamically. Refactored and organized CSS styles by reordering and adding new classes for better layout consistency and component styling. This enhances code maintainability and improves page rendering.

--- app/material/category/[slug]/page.tsx
+++ app/material/category/[slug]/page.tsx
@@ -28,7 +28,9 @@ async function getCategory(slug: string) {
     return taxRes.json();
 }
 
-export async function generateMetadata({params}: { params: { slug: string } }, parent: ResolvingMetadata): Promise<Metadata> {
+export async function generateMetadata({params}: {
+    params: { slug: string }
+}, parent: ResolvingMetadata): Promise<Metadata> {
 
     return {
         title: "Western Dakota Regional Water System - " + parseTitleFromSlug(params.slug),
@@ -55,6 +57,15 @@ async function getData(slug: string) {
         totalPages: res.headers.get("x-wp-totalpages")
     };
 }
+async function getPageData(slug: string) {
+    const res = await fetch(`${CMS_URL}pages?slug=${slug}`, { cache: 'no-store' })
+
+    if (!res.ok) {
+        notFound();
+    }
+
+    return res.json();
+}
 
 export default async function Page({params}: { params: { slug: string } }) {
     const data = await getData(params.slug);
@@ -62,10 +73,16 @@ export default async function Page({params}: { params: { slug: string } }) {
     if (!data.data[0]) {
         notFound();
     }
+    const pageData = await getPageData(params.slug);
+    console.log(pageData);
 
     const items = await PostList(data.data, "material");
 
     return (
-        <InnerPageContent width="LG" isContent={false} title={parseTitleFromSlug(params.slug)} content={items}/>
+        <InnerPageContent width="LG" isContent={false} title={parseTitleFromSlug(params.slug)} content={
+            <div>
+                {pageData[0] && <div className="mt-6" dangerouslySetInnerHTML={{__html: pageData[0].content.rendered}}/>}
+                {items}
+            </div>}/>
     );
 }
