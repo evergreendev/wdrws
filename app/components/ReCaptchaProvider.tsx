0a43547ad4b8f5443598b35472b9dcb52a2a4c6c
Joe simpson <joe@egmrc.com>
2026-02-02 09:40:03 -0700

    Add `ReCaptchaProvider` component to wrap layout with Google reCAPTCHA v3 support

---
+++ app/components/ReCaptchaProvider.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import React from "react";
+import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
+
+export default function ReCaptchaProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <GoogleReCaptchaProvider
+      reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY ?? ""}
+    >
+      {children}
+    </GoogleReCaptchaProvider>
+  );
+}
