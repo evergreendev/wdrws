96e342d9718f4243582d34cfe57718f503276e20
joe <joe@egmrc.com>
2026-08-10 15:12:13 -0600

    Add redirects

---
+++ app/AM26/route.ts
@@ -0,0 +1,5 @@
+import {redirect} from "next/navigation";
+
+export async function GET() {
+    redirect("https://docs.google.com/forms/d/e/1FAIpQLSetBkAGY0F3F20mvsJ9djt3IY8bNax00sldk6QvjjnKNJrasg/viewform?usp=header");
+}
---
+++ app/waterwatch/route.ts
@@ -0,0 +1,5 @@
+import {redirect} from "next/navigation";
+
+export async function GET() {
+    redirect("/western-dakota-regional-water-watch");
+}
